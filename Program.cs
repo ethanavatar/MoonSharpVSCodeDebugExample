45734879650b1eefc47f7cb6a603edab2fb39881
Ethan Evans <ethanalexevans@gmail.com>
2023-10-25 12:29:28 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MoonSharp.Interpreter;
+using MoonSharp.VsCodeDebugger;
+
+var debugger = new MoonSharpVsCodeDebugServer();
+debugger.Start();
+
+var script = new Script();
+script.Options.DebugPrint = s => Console.WriteLine(s);
+script.Globals["sleep"] = (Action<int>) Thread.Sleep;
+
+var cwd = AppDomain.CurrentDomain.BaseDirectory;
+var friendlyPath = Path.Join(cwd, "test.lua");
+var main = script.LoadFile("test.lua", friendlyFilename: friendlyPath);
+
+debugger.AttachToScript(script, "test");
+script.Call(main);
+
+debugger.Detach(script);
+debugger.Dispose();
---
+++ test.lua
@@ -0,0 +1,7 @@
+
+local i = 1
+while (1 == 1) do
+	print(i)
+	i = i + 1
+	sleep(1000)
+end
